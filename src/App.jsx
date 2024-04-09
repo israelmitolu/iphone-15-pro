5516edf7caf6e67c23196bec1e426a48a031baf6
israelmitolu <israelmitolu@gmail.com>
2024-04-09 21:06:15 +0100

    feat: Add Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 };
