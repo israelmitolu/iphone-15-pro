f955166eca18e87d33f36b9e2c086097830b62f7
israelmitolu <israelmitolu@gmail.com>
2024-04-09 20:55:46 +0100

    feat: Create How it works section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
